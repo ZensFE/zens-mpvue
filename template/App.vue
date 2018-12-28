74fa95e6700222c9f95622a263fb1ba766a9a921
wangchong <idoodle@aliyun.com>
2018-12-28 19:34:18 +0800

    fix:  基本跑通，可以逐渐完善了

---
+++ template/App.vue
@@ -0,0 +1,91 @@
+<script>
+  // import utils from './utils/utils.js'
+  import global from './global/global.js'
+
+  export default {
+    data () {
+      return {
+        code: ''
+      }
+    },
+    onShow (options) {
+
+    },
+    onLaunch (options) {
+      global.scene = options.scene
+      // global.wxLoginPromise = utils.wxLogin()
+      if (wx.canIUse('getUpdateManager')) {
+        const updateManager = wx.getUpdateManager()
+        updateManager.onCheckForUpdate(function (res) {
+          if (res.hasUpdate) {
+            updateManager.onUpdateReady(function () {
+              wx.showModal({
+                title: '更新提示',
+                content: '新版本已经准备好，是否重启应用？',
+                success: function (res) {
+                  if (res.confirm) {
+                    updateManager.applyUpdate()
+                  } else if (res.cancel) {
+                    updateManager.applyUpdate()
+                  }
+                }
+              })
+            })
+            updateManager.onUpdateFailed(function () {
+              wx.showModal({
+                title: '更新提示',
+                content: '新版本已经上线，请您删除当前小程序，重新搜索哲品有礼'
+              })
+            })
+          }
+        })
+      } else {
+        return false
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import "~z-icon/iconfont.css";
+  @import "./styles/zens-ui.plus.scss";
+  @import "./styles/common.scss";
+
+  .container {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    box-sizing: border-box;
+  }
+  /* this rule will be remove */
+  * {
+    transition: width 2s;
+    -moz-transition: width 2s;
+    -webkit-transition: width 2s;
+    -o-transition: width 2s;
+  }
+  page {
+    background: #eee;
+  }
+  .text-center {
+    text-align: center;
+  }
+  .clearfix:after {
+    display: block;
+    height: 0px;
+    visibility: hidden;
+    content: "";
+    clear: both;
+  }
+  button::after {
+    border: none;
+  }
+  .padding-bottom {
+    padding-bottom: 50rpx !important;
+  }
+  img {
+    display: block;
+  }
+</style>
--- template/src/pages/home/index.vue
+++ template/src/pages/home/index.vue
@@ -1,9 +1,14 @@
 <template>
-  <div>
-    <!--闪屏页-->
+  <div class="zens-container">
+    <img src="https://zens-pic.oss-cn-shenzhen.aliyuncs.com/static/space/in1.jpg"
+         mode="widthFix"
+         class="dummy-img" alt="">
 
-    <!--非闪屏区域-->
-
+    <div class="flex-container padding30">
+      <button class="btn">主色</button>
+      <button class="btn red pill">红色</button>
+      <button class="btn gray radius">授权页</button>
+    </div>
   </div>
 </template>
 
@@ -17,17 +22,12 @@ export default {
       welcomeShow: true
     }
   },
-  methods: {},
   onShow () {
-    // 判断是否需要展示【闪屏】，
-    this.welcomeShow = !this.isVisited()
-    if (this.welcomeShow) {
-      setTimeout(() => {
-        this.welcomeHideHandler()
-      }, 3000)
-    }
-    // 获取门店ID
-    this.initAll()
+    wx.showModal({
+      title: '注意',
+      showCancel: false,
+      content: 'App.vue中的utils.wxLogin()已被注释'
+    })
   }
 }
 </script>
@@ -37,30 +37,7 @@ export default {
   }
 </style>
 <style scoped lang="scss">
-
-  span.arrow-down::after {
-    /*content: '▲';*/
-    content: '▼';
-    font-size: 0.8em;
-    line-height: 1.2em;
-    vertical-align: center;
-    margin-left: 0.5em;
-  }
-  .banner-button > img {
-    border: none;
-  }
-  .left-tips {
-    display: inline-block;
-    background-color: transparentize(#000000, .6);
-    position: absolute;
-    top: 30rpx;
-    border-top-right-radius: 50rpx;
-    border-bottom-right-radius: 50rpx;
-  }
-  .click:active {
-    transform: scale(0.9875);
-  }
-  .click-l:active {
-    transform: scale(1.1);
+  img {
+    width: 100%;
   }
 </style>
