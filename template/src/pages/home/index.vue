c120824c28b76de006b739e5d5eb4f09890a6267
wangchong <idoodle@aliyun.com>
2018-12-30 20:10:38 +0800

    enhance: 首页追加更多的示例代码

--- template/src/pages/home/index.vue
+++ template/src/pages/home/index.vue
@@ -1,13 +1,29 @@
 <template>
   <form report-submit="true" @submit="formIdCollect" class="zens-container">
     <img src="https://zens-pic.oss-cn-shenzhen.aliyuncs.com/static/space/in1.jpg"
-         mode="widthFix"
+         mode="widthFix" style="width: 100%;"
          class="dummy-img" alt="">
 
     <div class="flex-container padding30">
       <button form-type="submit" class="btn">主色</button>
       <button class="btn red pill">红色</button>
       <button class="btn gray radius">授权页</button>
+    </div>
+
+    <div class="flex-container padding30">
+      <div class="btn-group">
+        <button class="btn pill">左侧按钮</button>
+        <button class="btn gray pill">右侧按钮</button>
+      </div>
+    </div>
+    <div class="flex-container padding30">
+      <text class="icon-home"></text>
+      <text class="icon-gift text-red"></text>
+      <text class="icon-buys text-primary"></text>
+      <text class="icon-close-circle text-darker"></text>
+      <text class="icon-coupon text-gray"></text>
+      <text class="icon-plus fs-40"></text>
+      <text class="icon-reduce fs-40"></text>
     </div>
   </form>
 </template>
@@ -36,7 +52,5 @@ export default {
   }
 </style>
 <style scoped lang="scss">
-  img {
-    width: 100%;
-  }
+  
 </style>
