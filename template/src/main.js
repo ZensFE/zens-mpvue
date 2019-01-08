256dca57b1e4d12af898991d5696dc9eaece31b1
wangchong <idoodle@aliyun.com>
2019-01-08 16:47:33 +0800

    fix： main.js追加锚点

--- template/src/main.js
+++ template/src/main.js
@@ -23,8 +23,10 @@ export default {
   config: {
     // 页面前带有 ^ 符号的，会被编译成首页，其他页面可以选填，我们会自动把 webpack entry 里面的入口页面加进去
     pages: [
+      // 此行注释勿删
       '^pages/sample/main',
       'pages/authorization/main'
+      // 此行注释勿删
     ],
     window: {
       backgroundTextStyle: 'light',
